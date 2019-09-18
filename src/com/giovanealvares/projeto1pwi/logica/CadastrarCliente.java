dbb222e3610af8a89fa73ba2c3f9e7031601f35c
Giovane Alvares <50254355+GiovaneA@users.noreply.github.com>
2019-09-18 14:50:28 -0300

    Correcoes

--- src/com/giovanealvares/projeto1pwi/logica/CadastrarCliente.java
+++ src/com/giovanealvares/projeto1pwi/logica/CadastrarCliente.java
@@ -1,12 +1,18 @@
 package com.giovanealvares.projeto1pwi.logica;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.giovanealvares.projeto1pwi.model.Cliente;
 import com.giovanealvares.projeto1pwi.model.PessoaFisica;
 import com.giovanealvares.projeto1pwi.dao.ClienteDAO;
 
-public class CadastrarCliente {
+public class CadastrarCliente implements Logica {
 	
+
 	@Override
 	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String nome = req.getParameter("nome");
