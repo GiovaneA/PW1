3695c9a5217b0fe8a8f0fc81b88e5ceea2566820
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-09-27 13:47:55 -0300

    Conta Poupança J
    Criação conta poupança Juridica

---
+++ src/com/giovanealvares/projeto1pwi/logica/CriarContaPoupancaJ.java
@@ -0,0 +1,34 @@
+package com.giovanealvares.projeto1pwi.logica;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.giovanealvares.projeto1pwi.dao.ProdutoDAO;
+import com.giovanealvares.projeto1pwi.model.ContaPoupanca;
+import com.giovanealvares.projeto1pwi.model.PessoaJuridica;
+
+public class CriarContaPoupancaJ implements Logica {
+
+	@Override
+	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int valor = Integer.parseInt(req.getParameter("valor"));
+		int idCliente = Integer.parseInt(req.getParameter("idCliente"));
+		
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		
+		String random = Integer.toString(1234567+(int)(8765433*Math.random()));
+		while(produtoDAO.listarNumerosPJ(random)){
+		random = Integer.toString(1234567+(int)(8765433*Math.random()));
+	}
+		ContaPoupanca cp = new ContaPoupanca(random);
+		cp.setSaldo(valor);
+		cp.setSituacao(true);
+		cp.setTipo(2);
+		produtoDAO.criarContaPoupancaJ(cp, idCliente);
+		return "/PaginaInicial.jsp";
+	}
+
+}
