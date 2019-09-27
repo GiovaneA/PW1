fa30744a9822f9d6b305ad21ddaed423e1a83318
FoppaHiago <49573560+FoppaHiago@users.noreply.github.com>
2019-09-27 14:07:00 -0300

    Criação SacarJ
    Criação classe sacar pra pessoa Juridica

---
+++ src/com/giovanealvares/projeto1pwi/logica/SacarJ.java
@@ -0,0 +1,23 @@
+package com.giovanealvares.projeto1pwi.logica;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.giovanealvares.projeto1pwi.dao.ProdutoDAO;
+
+public class SacarJ implements Logica {
+
+	@Override
+	public String executa(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//Conta conta = (Conta) req.getAttribute("conta");
+		int idConta = Integer.parseInt(req.getParameter("id"));
+		double valor = Double.parseDouble(req.getParameter("valor"));
+		ProdutoDAO produtoDAO = new ProdutoDAO();
+		produtoDAO.sacarJ(idConta, valor);
+		return "PaginaInicial.jsp";
+	}
+
+}
